5e921ad84daf2f15082f0f263fc303817b75c3c2
zolezzi <cezcardozo23@gmail.com>
2023-09-17 02:37:44 -0300

    Adding project skeleton

---
+++ src/main/java/cl/nisum/userchallenge/UserchallengeApplication.java
@@ -0,0 +1,13 @@
+package cl.nisum.userchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/nisum/userchallenge/UserchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.nisum.userchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
